5bbf246b4e02dc77079a96a3fa96b6c01d23d817
mandeepdhiman123 <mandeep.kumar@technoforte.co.in>
2022-04-07 19:52:08 +0530

    added docker file

--- resident-ui/configure_start.sh
+++ resident-ui/configure_start.sh
@@ -4,16 +4,16 @@
 set -e
 
 echo "Downloading pre-requisites install scripts"
-wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifactory/libs-release-local/i18n/admin-i18n-bundle.zip -O $i18n_path/admin-i18n-bundle.zip
-wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifactory/libs-release-local/i18n/admin-entity-spec-bundle.zip -O $entity_spec_path/admin-entity-spec-bundle.zip
+wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifactory/libs-release-local/i18n/admin-i18n-bundle.zip -O $i18n_path/resident-i18n-bundle.zip
+wget --no-check-certificate --no-cache --no-cookies $artifactory_url_env/artifactory/libs-release-local/i18n/admin-entity-spec-bundle.zip -O $entity_spec_path/resident-entity-spec-bundle.zip
 
 echo "unzip pre-requisites.."
 chmod 775 $i18n_path/*
 
 cd $entity_spec_path
-unzip -o admin-entity-spec-bundle.zip
+unzip -o resident-entity-spec-bundle.zip
 cd $i18n_path
-unzip -o admin-i18n-bundle.zip
+unzip -o resident-i18n-bundle.zip
 
 echo "unzip pre-requisites completed."
 
