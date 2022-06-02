e5a3ea1af156a699d45ed7b84d91fe48119ad00f
Aravindhan Alagesan <aravindh2090@gmail.com>
2022-06-02 17:56:44 +0530

    Changes related to latest config

--- resident-ui/src/app/shared/shared.module.ts
+++ resident-ui/src/app/shared/shared.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
+import { DialogComponent } from './dialog/dialog.component';
 import { HeaderComponent } from './header/header.component';
 import { I18nModule } from 'src/app/i18n.module';
 
 @NgModule({
   imports: [CommonModule, MaterialModule, ReactiveFormsModule, FormsModule, I18nModule],
-  declarations: [ HeaderComponent ],
-  exports: [ MaterialModule, HeaderComponent ],
-  entryComponents: [],
+  declarations: [DialogComponent, HeaderComponent ],
+  exports: [DialogComponent, MaterialModule, HeaderComponent ],
+  entryComponents: [DialogComponent],
   providers: []
 })
 export class SharedModule {}
