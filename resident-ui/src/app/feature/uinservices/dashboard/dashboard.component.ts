2f81f937dad262874fff0ef03e22fa241d7f3f45
Madhuravas <madhuravasreddy1998@gmail.com>
2023-04-06 11:21:16 +0530

    fixed log out when we click browser back button

--- resident-ui/src/app/feature/uinservices/dashboard/dashboard.component.ts
+++ resident-ui/src/app/feature/uinservices/dashboard/dashboard.component.ts
@@ -5,6 +5,9 @@ import { Subscription } from "rxjs";
 import { Router } from "@angular/router";
 import { AutoLogoutService } from "src/app/core/services/auto-logout.service";
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { LogoutService } from 'src/app/core/services/logout.service';
+import { AuditService } from 'src/app/core/services/audit.service';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: "app-uindashboard",
@@ -14,11 +17,19 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 export class DashboardComponent implements OnInit, OnDestroy {
   menuItems: any;
   subscriptions: Subscription[] = [];
-  message:any;
-  cols : number;
+  message: any;
+  cols: number;
   userPreferredLangCode = localStorage.getItem("langCode");
 
-  constructor(private autoLogout: AutoLogoutService, private dataStorageService: DataStorageService, private translateService: TranslateService, private router: Router, private breakpointObserver: BreakpointObserver) { 
+  constructor(
+    private autoLogout: AutoLogoutService,
+    private dataStorageService: DataStorageService,
+    private translateService: TranslateService,
+    private router: Router,
+    private breakpointObserver: BreakpointObserver,
+    private logoutService: LogoutService,
+    private auditService: AuditService
+  ) {
     this.breakpointObserver.observe([
       Breakpoints.XSmall,
       Breakpoints.Small,
@@ -70,6 +81,23 @@ export class DashboardComponent implements OnInit, OnDestroy {
     }
   }
 
+  @HostListener('window:popstate', ['$event'])
+  PopState(event) {
+    console.log("Testing1")
+    console.log(window.location.hash)
+    if (window.location.hash.includes("uinservices")) {
+      console.log("Testing2")
+    } else {
+      console.log("Testing3")
+      if (confirm("Are you sure want to leave the page. you will be logged out automatically if you press OK?")) {
+        this.auditService.audit('RP-002', 'Logout', 'RP-Logout', 'Logout', 'User clicks on "logout" button after logging in to UIN services');
+        this.logoutService.logout();
+      } else {
+        this.router.navigate([this.router.url]);
+        return false;
+      }
+    }
+  }
   ngOnDestroy(): void {
     this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
